4d7448e5504e35a14b71ad27c8fa6844436b6e2d
jnsct <56279432+jnsct@users.noreply.github.com>
2020-08-30 18:01:38 -0400

    Improved array management, using jagged array
    Reworked the way data is stored in order to use a jagged array, focus on using pointers to pointers instead of a 2D array.

--- decode.c
+++ decode.c
@@ -5,9 +5,10 @@
 
 struct info{
 	int X, Y; // Size of X and Y (resolution)
-	int* xArr;
-	int* yArr; // Nonogram values of X and Y
-};
+	int *xArr;
+	int *yArr; // Nonogram values of X and Y
+} info;
+
 
 typedef struct info Struct;
 
@@ -17,61 +18,82 @@ Struct readFile(char* fileName){
 	s.Y = 0;
 	FILE *inPtr;
 	inPtr = fopen(fileName, "r");
+	int num;
 	if (inPtr == NULL){
 		printf("ERROR: FILE NOT FOUND\n");
 		return s;
 	}
-	int tempX = (int) fgetc(inPtr) - '0';
-	int tempY = (int) fgetc(inPtr) - '0';
-
-	for(int i = 0; i < tempX+tempY; i++){
-		if(i < tempX){
-			s.X += (fgetc(inPtr) - '0') * ((int) pow(10, (tempX - i - 1)));
-		}
-		else{
-			s.Y += (fgetc(inPtr) - '0') * ((int) pow(10, (tempY - i - 1 + tempX)));
+	const int X = (int) fgetc(inPtr) - '0'; // Magnitude of X
+	const int Y = (int) fgetc(inPtr) - '0'; // Magnitude of Y
+	const int MSUB = (int) fgetc(inPtr) - '0'; // Magnitude of the size of the largest substring in x
+	int i = 0;
+	while(num != -38){
+		num = fgetc(inPtr) - '0';
+		if(num != -38) {
+			if (i < X) {
+				s.X += num * ((int) pow(10, (X - i - 1)));
+			}
+			else{
+				s.Y += num * ((int) pow(10, (Y - i - 1 + X)));
+			}
+			i++;
 		}
 	}
-	s.xArr = malloc(s.X * sizeof(int));
-	s.yArr = malloc((200)* sizeof(int));
-
+	int *xArr = calloc(s.X, sizeof(int));
+	int *yArr = calloc(s.Y, sizeof(int));
+	int hold = -1;
 	int EOFCheck = 0;
-	int EOA = 0; //end of array
-	int num;
-	int n = 0;
-	int* subArr = malloc((200)* sizeof(int));
-	fgetc(inPtr);
+	int n = -1;
 	while(!EOFCheck){
 		num = (int) fgetc(inPtr) - '0';
+		// catching the end of the line
 		if(num == -38){
 			n++;
 		}
+		// catching the end of the file
 		else if(num == -49){
 			EOFCheck = 1;
 		}
+		// processing subarrays
 		else if(num == -4){
-			if(n < s.X){
-				subArr[0] = s.xArr[n-1];
+			int *tSArr =  calloc(MSUB, sizeof(int)); //temporary subarray
+			tSArr[0] = hold;
+			fgetc(inPtr) - '0';
+			i = 1;
+			while(num >= 0){
+				tSArr[i] = num;
+				num = (int) fgetc(inPtr) - '0';
+				i++;
+			}
+			int *sArr = malloc(2 * sizeof(int)); //final subarray
+			for(int k = 0; k < i; k++){
+				sArr[k] = tSArr[k];
+			}
+			free(tSArr);
+		}
+
+
+		// adding individual values to array
+		else{
+			if (n < s.X) {
+				if(hold != -1){
+					xArr[n] = (int *) hold;
+					hold = num;
+				}
+				else{hold = num;}
+
 			}
 			else{
-				//Figure out why it only works with n-8 (2^3?)
-				subArr[0] = s.yArr[n-8];
+				if(hold != -1){
+					yArr[n - s.X] = (int*) hold;
+					hold = num;
+				}
+				else{hold = num;}
 			}
-			int i = 1;
-			while(num != -38){
-				num = (int) fgetc(inPtr) - '0';
-				subArr[i] = num;
-				num = (int) fgetc(inPtr) - '0';
-				printf("%i,%i, num is %i\n",subArr[0],subArr[1], num);
-			}
-		}
-		else if(n < s.X){
-			s.xArr[n] = num;
-		}
-		else{
-			s.yArr[n - s.X] = num;
 		}
 	}
+	s.xArr = xArr;
+	s.yArr = yArr;
 	return s;
 
 }
@@ -97,12 +119,7 @@ int main() {
 
 	printf("Resolution is %ix%i\n", s.X, s.Y);
 
-
-	for(int i = 0; i < s.X; i++){
-		printf("%i ", s.xArr[i]);
-	}
-	printf("\n");
-	for(int i = 0; i < s.Y; i++){
-		printf("%i ", s.yArr[i]);
+	for(int i = 0; i<8; i++) {
+		printf("%i: %i\n", i, s.xArr[i]);
 	}
 }
